7b76ed8f0ac3582fedd5d49d6e8312beca088fe4
mosetf <stanlym315@gmail.com>
2024-01-18 17:21:27 +0300

    100-average_weighted_score.sql

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,14 @@
+-- script that creates a stored procedure ComputeAverageWeightedScoreForUsers
+-- that computes and store the average weighted score for all students
+DROP procedure IF EXISTS ComputeAverageWeightedScoreForUser;
+DELIMITER //
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (
+	IN user_id INT
+)
+BEGIN
+    UPDATE users
+   	SET average_score=(SELECT AVG(score) FROM corrections
+			     WHERE corrections.user_id=user_id)
+	WHERE id=user_id;
+END;
+//
