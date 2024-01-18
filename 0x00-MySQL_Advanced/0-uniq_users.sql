47db62484a70112fd77ed39ef36f4b6e26b116d0
mosetf <stanlym315@gmail.com>
2024-01-18 16:03:18 +0300

    fix create table user script 1

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,7 @@
---creates table users
---with attributes id, email, name
+-- script that creates table users
+-- with attributes id, email, name
 
-CREATE TABLE IF NOT EXISTS users
-(
+CREATE TABLE IF NOT EXISTS users(
 	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email varchar(255) NOT NULL UNIQUE,
 	name varchar(255)
