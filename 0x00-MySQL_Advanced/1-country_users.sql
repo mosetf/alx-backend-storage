dfba432ee356a76bf19d45bf0f3d207e5aea0644
mosetf <stanlym315@gmail.com>
2024-01-18 16:32:59 +0300

    fix 0-uniq_users.sql 3

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,7 @@
 -- script that creates a table users
--- with columns id, email, name, country
-
 CREATE TABLE IF NOT EXISTS users(
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email varchar(255) NOT NULL UNIQUE,
     name varchar(255),
-    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL,
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
 );
