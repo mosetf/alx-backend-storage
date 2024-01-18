593a854f64739e36ae1071d33626919125a61db3
mosetf <stanlym315@gmail.com>
2024-01-18 16:27:25 +0300

    fix 0-uniq_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,4 +1,4 @@
---  script that creates a table users
+-- script that creates a table users
 CREATE TABLE IF NOT EXISTS users(
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email varchar(255) NOT NULL UNIQUE,
