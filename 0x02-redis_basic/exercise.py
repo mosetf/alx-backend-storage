b9d93273e988d7adf183c1c15dd889566584d629
mosetf <stanlym315@gmail.com>
2024-01-24 18:56:37 +0300

    implement count calls method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,33 +2,19 @@
 """Exercise for Redis"""
 import redis
 import uuid
+from typing import Union, Callable, Optional
+from functools import wraps
 
 
-class Cache:
-    """Cache class"""
-    def __init__(self):
-        """Constructor"""
-        self._redis = redis.Redis()
-        self._redis.flushdb()
+def count_calls(method: Callable) -> Callable:
+    """Decorator count number of calls"""
+    key = method.__qualname__
 
-    def store(self, data: bytes) -> str:
-        """Store data in Redis"""
-        key = str(self._redis.incr("count:{}".format(data)), "utf-8")
-        self._redis.set(key, data)
-        return key
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """Wrapper function"""
+        self._redis.incr(key)
+        return method(self, *args, **kwds)
+    return wrapper
 
-    def get(self, key: str, fn: object) -> str:
-        """Get data from Redis"""
-        data = self._redis.get(key)
-        if data:
-            return fn(data)
-        return None
 
-    def get_str(self, data: bytes) -> str:
-        """Convert bytes to string"""
-        return data.decode("utf-8")
-
-    def get_int(self, data: bytes) -> int:
-        """Convert bytes to int"""
-        return int.from_bytes(data, "big")
-        
