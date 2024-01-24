33591d4beec92f92b641697d74cddb77980c1171
mosetf <stanlym315@gmail.com>
2024-01-24 19:00:49 +0300

    update the store method in class cache

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -57,3 +57,13 @@ class Cache:
         """Constructor"""
         self._redis = redis.Redis()
         self._redis.flushdb()
+
+    @call_history
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Store data in Redis"""
+        key = str(uuid.uuid4())
+        self._redis.mset({key: data})
+        return key
+
+
