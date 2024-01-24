e458cd6d0bbf18d583160cbd98792b80dd909465
mosetf <stanlym315@gmail.com>
2024-01-24 18:57:17 +0300

    implement call_history

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,3 +18,19 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """Decorator store the history of inputs and outputs"""
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """Wrapper function"""
+        input = str(args)
+        self._redis.rpush("{}:inputs".format(key), input)
+
+        output = str(method(self, *args, **kwds))
+        self._redis.rpush("{}:outputs".format(key), output)
+
+        return output
+    return wrapper
+
