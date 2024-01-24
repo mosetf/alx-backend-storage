94c8044877f439f71f8062e6be31725011eff7d4
mosetf <stanlym315@gmail.com>
2024-01-24 18:41:56 +0300

    create Cache class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""Exercise for Redis"""
+import redis
+import uuid
+
+
+class Cache:
+    """Cache class"""
+    def __init__(self):
+        """Constructor"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
