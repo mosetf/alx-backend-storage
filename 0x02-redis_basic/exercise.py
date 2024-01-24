8f4a8787922fbe22f6c517c40fe1a2cca58d1d22
mosetf <stanlym315@gmail.com>
2024-01-24 18:58:31 +0300

    'implement decorator to display history of calls

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -34,3 +34,19 @@ def call_history(method: Callable) -> Callable:
         return output
     return wrapper
 
+
+def replay(method: Callable):
+    """Decorator display the history of calls of a particular function"""
+    r = redis.Redis()
+    key = method.__qualname__
+
+    inputs = r.lrange("{}:inputs".format(key), 0, -1)
+    outputs = r.lrange("{}:outputs".format(key), 0, -1)
+
+    print("{} was called {} times:".format(key, r.get(key).decode("utf-8")))
+
+    for i, o in zip(inputs, outputs):
+        print("{}(*{}) -> {}".format(key, i.decode("utf-8"),
+                                     o.decode("utf-8")))
+    return method
+
